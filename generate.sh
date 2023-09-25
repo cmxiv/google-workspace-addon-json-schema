7f9c27a31d1ae8d7cab544a4591f2586c2478256
Siddhant Tandon <cmxiv914@gmail.com>
2023-09-25 20:43:47 +0200

    Add generate.sh to improve the type generation

---
+++ generate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+if [ ! -d "./generated" ]; then
+  echo "📂 Creating folders to generate file"
+  mkdir generated
+fi
+
+if [ ! -d "./generated/$1" ]; then
+  echo "📂 Creating sub-folder for $1 types"
+  mkdir generated/"$1"
+fi
+
+FLAGS=""
+if test "$1" = "typescript"; then
+  FLAGS="--just-types"
+fi
+
+echo "⏳  Generating types in 'generated' folder..."
+yarn run quicktype $FLAGS --lang "$1" -s schema --src SubmitFormResponse.schema.json -o generated/"$1"/SubmitFormResponse."$2"
+yarn run quicktype $FLAGS --lang "$1" -s schema --src Card.schema.json -o generated/"$1"/Card."$2"
+yarn run quicktype $FLAGS --lang "$1" -s schema --src CommonEvent.schema.json -o generated/"$1"/Event."$2"
+yarn run quicktype $FLAGS --lang "$1" -s schema --src Deployment.schema.json -o generated/"$1"/Deployment."$2"
+
+echo "✅  Types generated!"
