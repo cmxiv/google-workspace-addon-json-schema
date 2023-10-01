24c510b2558c2f64fba6707d89103f60d925167c
Siddhant Tandon <cmxiv914@gmail.com>
2023-10-02 01:24:19 +0200

    Add index.d.ts while generating typescript types

--- generate.sh
+++ generate.sh
@@ -16,6 +16,12 @@ if test "$1" = "typescript"; then
   quicktype --just-types --lang typescript -s schema --src RenderActionResponse.schema.json --src Card.schema.json -o generated/"$1"/Card.ts
   quicktype --just-types --lang typescript -s schema --src CommonEvent.schema.json -o generated/"$1"/Event.ts
   quicktype --just-types --lang typescript -s schema --src Deployment.schema.json -o generated/"$1"/Deployment.ts
+
+  ALL_EVENT_EXPORTS=$(cat generated/"$1"/Event.ts | grep "export" | grep -v "ConferenceSolution" | awk '{print $3}' ORS=',' | awk '{print substr($1, 1, length($1)-1)}')
+  echo "export {$ALL_EVENT_EXPORTS} from \"./Event\"" > generated/"$1"/index.d.ts
+  echo "export {ConferenceSolution as ConferenceSolutionObject} from \"./Event\"" >> generated/"$1"/index.d.ts
+  echo "export * from \"./Deployment\"" >> generated/"$1"/index.d.ts
+  echo "export * from \"./Card\"" >> generated/"$1"/index.d.ts
 else
   quicktype --lang "$1" -s schema --src SubmitFormResponse.schema.json -o generated/"$1"/SubmitFormResponse."$2"
   quicktype --lang "$1" -s schema --src Card.schema.json -o generated/"$1"/Card."$2"
